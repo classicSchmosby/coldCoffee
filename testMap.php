45a05721eeaafb740eab74b23d69b72b3613fa7b
Lee Grix <leegrix98@gmail.com>
2017-08-31 22:15:51 +0100

    Progress on MySQL populated map
    Lots more work needs to be done though...

--- testMap.php
+++ testMap.php
@@ -9,9 +9,18 @@
     <title>Simple Map</title>
     <meta name="viewport" content="initial-scale=1.0">
     <meta charset="utf-8">
-    <style>
+    <script src="assets/js/adsbygoogle.js" type="text/javascript"></script>
+    <script src="assets/js/analytics.js" type="text/javascript"></script>
+    <script src="assets/js/googleAsync.js" type="text/javascript"></script>
+    <script src="assets/js/jquery.min.js" type="text/javascript"></script>
+
+    <style type="text/css">
       #map {
-        height:100%;
+        height:800px;
+        width:800px;
+        border:1px solid black;
+        margin-left:10px;
+        margin-top:10px;
       }
       html, body {
         width:100%;
@@ -22,53 +31,47 @@
     </style>
   </head>
   <body>
+
     <div id="map"></div>
-    <script>
+    
+  </body>
+  <script type="text/javascript">
       function initMap() {
+        var infowindow = new google.maps.InfoWindow();
         var map = new google.maps.Map(document.getElementById('map'), {
           center: new google.maps.LatLng(52.595284, 0.80887),
-          zoom: 8
+          zoom: 8,
+          mapTypeControl: false,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
         });
+        
+
+        function placeMarker(loc) {
+          var latLng = new google.maps.LatLng(<?php echo($row['lat']); ?>, <?php echo($row['lng']); ?>);
+          var marker = new google.maps.Marker({
+            position: latLng,
+            map: map
+          });
+          google.maps.event.addListener(marker, 'click', function(){
+            infowindow.close(); // Close previously opened infowindow
+            infowindow.setContent( "<div id='infowindow'>"+ <?php echo($row['locationId']); ?> +"</div>");
+            infowindow.open(map, marker);
+          });
+        }
 
         <?php
-          $markerQuery = mysqli_query($conn,
-            "SELECT * FROM TABLE");
-          // not needed with while? - $row = mysqli_fetch_array($markerQuery, MYSQLI_ASSOC);
-          // now you can: echo($row['COLUMN_NAME']);
-
-          while($map = mysqli_fetch_assoc($markerQuery)) :
-            $markerId = $map['markerId'];
-            $markerDesc = $map['markerDesc'];
-            $markerWebsite = $map['markerWebsite'];
-            $markerTitle = $map['markerTitle'];
-            $lat = $map['lat'];
-            $lng = $map['lng'];
-            $markerMap = $map['markerMap'];
-            $markerIcon = $map['markerIcon'];
+          $markerQuery = mysqli_query($conn, "SELECT * FROM location WHERE (hidden = 0)");
+          while($row = mysqli_fetch_array($markerQuery, MYSQLI_ASSOC)):
         ?>
-        var marker = new google.maps.Marker({
-          id: <?php echo($markerId); ?>,
-          description: <?php echo($markerDesc); ?>,
-          website: <?php echo($markerWebsite); ?>,
-          title: <?php echo($markerTitle); ?>,
-          position: new google.maps.LatLng(<?php echo($lat); ?>, <?php echo($lng); ?>),
-          map: <?php echo($markerMap); ?>,
-          icon: <?php echo($markerIcon); ?>
-        });
+          placeMarker(<?php echo($row); ?>);
+        
+        // Try this?: https://stackoverflow.com/questions/3059044/google-maps-js-api-v3-simple-multiple-marker-example
+        
 
-        google.maps.event.addListener(marker, 'click', (function(marker) {
-          return function() {
-            infowindow.setContent('<div>Location ID: '+ <?php echo($markerId); ?> + '</div>');
-            infowindow.open(map, marker);
-          }
-      })(marker));
+        <?php endwhile; ?>
 
-        <?php 
-          endwhile;
-        ?>
       } // initMap
+      google.maps.event.addDomListener(window, 'load', initMap);
     </script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAomdRkAs6Es584dRUIflhwTTdcjkLwI_Y&callback=initMap"
-    async defer></script>
-  </body>
+      <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAomdRkAs6Es584dRUIflhwTTdcjkLwI_Y&callback=initMap" type="text/javascript"></script>
 </html>
