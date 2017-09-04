811a7cbd7e5fe979e36f069b1c8dcccd7e543da0
Lee Grix <leegrix98@gmail.com>
2017-09-04 12:06:05 +0100

    Redirect from Register
    Creating a redirect page where users will go from the register page, and then will be redirected to the home page.

---
+++ registerRedirect.php
@@ -0,0 +1,29 @@
+<!-- <?php
+	include('session.php');
+	header("Location: home.php");
+?> -->
+
+<!DOCTYPE html>
+<html style="background-color:#fff;">
+<head>
+	<link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon" />
+    <link href="assets/css/main.css" rel="stylesheet">
+
+    <script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/Chart.js"></script>
+	<script src="assets/js/map.js"></script>
+	<title>Test</title>
+</head>
+<body style="background-color:#fff;">
+
+<h2 class="mainText">You will be redirect to your profile page shortly.</h2>
+<h2 class="mainText">Your profile is being created. Please wait...</h2>
+
+<br />
+
+<div>
+	<img id="redirectLoader" src="assets/img/gears.gif">
+</div>
+
+</body>
+</html>
