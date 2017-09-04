ae0b72624e3d48250f11210a2798f09194969cbd
Lee Grix <leegrix98@gmail.com>
2017-09-04 14:09:13 +0100

    Register Redirect - improved
    Added more features to this page, to make it slightly more exciting for the user.

--- registerRedirect.php
+++ registerRedirect.php
@@ -1,6 +1,7 @@
 <!-- <?php
 	include('session.php');
-	header("Location: home.php");
+	//header("Location: home.php");
+	header("refresh:3;url=home.php");
 ?> -->
 
 <!DOCTYPE html>
@@ -12,18 +13,34 @@
     <script src="assets/js/jquery.min.js"></script>
 	<script src="assets/js/Chart.js"></script>
 	<script src="assets/js/map.js"></script>
-	<title>Test</title>
+	<title>Redirect</title>
 </head>
 <body style="background-color:#fff;">
 
-<h2 class="mainText">You will be redirect to your profile page shortly.</h2>
-<h2 class="mainText">Your profile is being created. Please wait...</h2>
+<h2 class="mainText">Welcome, <?php echo($login_session); ?></h2>
+<h2 class="mainText">You will be redirected to your profile page shortly.</h2>
+<h2 class="mainText">Your profile is being created. Please wait.<span id="wait"></span></h2>
 
 <br />
 
-<div>
+<div id="redirectLoaderWrapper">
 	<img id="redirectLoader" src="assets/img/gears.gif">
 </div>
 
 </body>
+<script type="text/javascript">
+window.dotsGoingUp = true;
+var dots = window.setInterval( function() {
+	var wait = document.getElementById("wait");
+	if ( window.dotsGoingUp ) 
+    	wait.innerHTML += ".";
+	else {
+    	wait.innerHTML = wait.innerHTML.substring(1, wait.innerHTML.length);
+    	if ( wait.innerHTML === "")
+        	window.dotsGoingUp = true;
+	}
+	if ( wait.innerHTML.length > 5 )
+    	window.dotsGoingUp = false;
+}, 300);
+</script>
 </html>
